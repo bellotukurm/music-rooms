f0a7bb142cc421180eee9d4011587ec6e1db7ce1
Muhammad Tukur <bellotukurm@gmail.com>
2024-08-31 19:40:21 +0100

    after tutorial 10

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -16,7 +16,8 @@ export default class HomePage extends Component {
     super(props);
     this.state = {
       roomCode: null,
-    }
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -51,6 +52,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    })
+  }
+
   render() {
     return (
       <Router>
@@ -64,7 +71,12 @@ export default class HomePage extends Component {
           }}/>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route 
+            path="/room/:roomCode" 
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }} 
+          />
         </Switch>
       </Router>
     );
